f34cfd7ce485585e6e67bf8487bc8f7fd627d800
05 Dewa Raditya Rochman <dewarochman26@gmail.com>
2024-10-03 10:51:13 +0700

    feat: add logo in browser tab

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-
+    <link rel="icon" href="{{ asset('img/apple-touch-icon.png') }}" type="image/png">
     @inertiaHead
     @viteReactRefresh
     @vite(['resources/js/app.jsx'])
